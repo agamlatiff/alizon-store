d8455030f86c82937576f3f247f8b29fcf852465
agamlatiff <agam.latiff@gmail.com>
2025-08-07 22:11:58 +0700

    Dashboard - Home

---
+++ src/app/(admin)/dashboard/(index)/_components/FormLogOut.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import type { ActionResult } from "@/types";
+import { LogOut } from "lucide-react";
+import React from "react";
+import { useFormState } from "react-dom";
+import Logout from "../lib/actions";
+
+const initialState: ActionResult = {
+  error: "",
+};
+
+const FormLogOut = () => {
+  const [state, formAction] = useFormState(Logout, initialState);
+
+  return (
+    <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <form action={formAction}>
+            <button className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8">
+              <LogOut className="h-5 w-5" />
+              <span className="sr-only">Logout</span>
+            </button>
+          </form>
+        </TooltipTrigger>
+        <TooltipContent side="right">Logout</TooltipContent>
+      </Tooltip>
+    </nav>
+  );
+};
+
+export default FormLogOut;
--- src/app/(admin)/dashboard/(index)/page.tsx
+++ src/app/(admin)/dashboard/(index)/page.tsx
@@ -1,5 +1,5 @@
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return <h1>DashboardPage</h1>;
 };
 
 export default DashboardPage;
