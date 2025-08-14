2472d63431baf60a94cc2e8cae1ef25b50f6dc4b
agamlatiff <agam.latiff@gmail.com>
2025-08-14 16:35:51 +0700

    Customer - Detail Product (2)

---
+++ src/app/(customers)/(index)/catalogs/detail-product/[id]/_components/CarouselImages.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Flickity from "react-flickity-component";
+
+const CarouselImages = () => {
+  return (
+    <>
+      <div id="details-images" className="main-carousel mt-[30px]">
+        <Flickity
+          options={{
+            cellAlign: "left",
+            contain: true,
+            pageDots: false,
+            prevNextButtons: false,
+          }}
+          className="carousel"
+        >
+          {[0, 1, 2].map((item) => (
+            <div key={item} className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
+              <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
+                <img
+                  src="/assets/thumbnails/color_back_green__buxxfjccqjzm_large_2x-Photoroom 1.png"
+                  className="w-full h-full object-contain"
+                  alt="thumbnail"
+                />
+              </div>
+            </div>
+          ))}
+        </Flickity>
+      </div>
+    </>
+  );
+};
+
+export default CarouselImages;
--- src/app/(customers)/(index)/catalogs/detail-product/[id]/page.tsx
+++ src/app/(customers)/(index)/catalogs/detail-product/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Suspense } from "react";
 import Navbar from "../../../_components/Navbar";
+import ListProducts from "../../../_components/ListProducts";
+import PriceInfo from "./_components/PriceInfo";
 
 const DetailProductPage = () => {
   return (
@@ -49,44 +52,7 @@ const DetailProductPage = () => {
           <p className="font-semibold">(4,389)</p>
         </div>
       </div>
-      <div id="details-images" className="main-carousel mt-[30px]">
-        <div className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
-          <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
-            <img
-              src="/assets/thumbnails/color_back_green__buxxfjccqjzm_large_2x-Photoroom 1.png"
-              className="w-full h-full object-contain"
-              alt="thumbnail"
-            />
-          </div>
-        </div>
-        <div className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
-          <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
-            <img
-              src="/assets/thumbnails/imac24-digitalmat-gallery-1-202310-Photoroom 1.png"
-              className="w-full h-full object-contain"
-              alt="thumbnail"
-            />
-          </div>
-        </div>
-        <div className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
-          <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
-            <img
-              src="/assets/thumbnails/246c3a1bf608fed816e2e038784fa995.png"
-              className="w-full h-full object-contain"
-              alt="thumbnail"
-            />
-          </div>
-        </div>
-        <div className="image-card pr-5 first-of-type:pl-[calc((100vw-1130px-20px)/2)]">
-          <div className="bg-white w-[470px] h-[350px] p-10 flex shrink-0 border border-[#E5E5E5] justify-center items-center rounded-[30px] overflow-hidden">
-            <img
-              src="/assets/thumbnails/ea49dfcfcaa4513d799050c989d2f177.png"
-              className="w-full h-full object-contain"
-              alt="thumbnail"
-            />
-          </div>
-        </div>
-      </div>
+
       <div
         id="details-benefits"
         className="container max-w-[1130px] mx-auto flex items-center gap-[50px] justify-center mt-[50px]"
@@ -341,196 +307,22 @@ const DetailProductPage = () => {
             </div>
           </div>
         </div>
-        <div className="w-[302px] flex flex-col shrink-0 gap-5 h-fit">
-          <div className="w-full bg-white border border-[#E5E5E5] flex flex-col gap-[30px] p-[30px] rounded-3xl">
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Brand New</p>
-              <p className="font-bold text-[32px] leading-[48px]">
-                Rp 56.500.000
-              </p>
-            </div>
-            <div className="flex flex-col gap-4">
-              <div className="flex items-center gap-2">
-                <div className="flex shrink-0">
-                  <img src="/assets/icons/tick-circle.svg" alt="icon" />
-                </div>
-                <p className="font-semibold">Peti telur packaging</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex shrink-0">
-                  <img src="/assets/icons/tick-circle.svg" alt="icon" />
-                </div>
-                <p className="font-semibold">Manual book instructions</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex shrink-0">
-                  <img src="/assets/icons/tick-circle.svg" alt="icon" />
-                </div>
-                <p className="font-semibold">Customer service 24/7</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex shrink-0">
-                  <img src="/assets/icons/tick-circle.svg" alt="icon" />
-                </div>
-                <p className="font-semibold">Free delivery Jababeka</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex shrink-0">
-                  <img src="/assets/icons/tick-circle.svg" alt="icon" />
-                </div>
-                <p className="font-semibold">Kwitansi orisinal 100%</p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-3">
-              <a
-                href="cart.html"
-                className="p-[12px_24px] bg-[#0D5CD7] rounded-full text-center font-semibold text-white"
-              >
-                Add to Cart
-              </a>
-              <a
-                href=""
-                className="p-[12px_24px] bg-white rounded-full text-center font-semibold border border-[#E5E5E5]"
-              >
-                Save to Wishlist
-              </a>
-            </div>
-          </div>
-          <a href="">
-            <div className="w-full bg-white border border-[#E5E5E5] flex items-center justify-between gap-2 p-5 rounded-3xl">
-              <div className="flex items-center gap-[10px]">
-                <div className="w-12 h-12 flex shrink-0 rounded-full bg-[#FFC736] items-center justify-center overflow-hidden">
-                  <img src="/assets/icons/cake.svg" alt="icon" />
-                </div>
-                <div className="flex flex-col gap-[2px]">
-                  <p className="font-semibold">Buy as a Gift</p>
-                  <p className="text-sm">Free Delivery</p>
-                </div>
-              </div>
-              <div className="flex shrink-0">
-                <img src="/assets/icons/arrow-right.svg" alt="icon" />
-              </div>
-            </div>
-          </a>
-        </div>
+        <PriceInfo />
       </div>
       <div
         id="recommedations"
         className="container max-w-[1130px] mx-auto flex flex-col gap-[30px] pb-[100px] mt-[70px]"
       >
-        <div className="flex items-center justify-between">
-          <h2 className="font-bold text-2xl leading-[34px]">
-            Other Products <br /> You Might Need
-          </h2>
-        </div>
-        <div className="grid grid-cols-5 gap-[30px]">
-          <a href="" className="product-card">
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <img
-                  src="/assets/thumbnails/color_back_green__buxxfjccqjzm_large_2x-Photoroom 1.png"
-                  className="w-full h-full object-contain"
-                  alt="thumbnail"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">
-                    iMac Green Energy
-                  </p>
-                  <p className="text-sm text-[#616369]">Desktops</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  Rp 24.000.000
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="" className="product-card">
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <img
-                  src="/assets/thumbnails/iphone15pro-digitalmat-gallery-3-202309-Photoroom 1.png"
-                  className="w-full h-full object-contain"
-                  alt="thumbnail"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">
-                    Smartwei Pro 18
-                  </p>
-                  <p className="text-sm text-[#616369]">Phones</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  Rp 11.000.000
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="" className="product-card">
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <img
-                  src="/assets/banners/mba13-m2-digitalmat-gallery-1-202402-Photoroom 2.png"
-                  className="w-full h-full object-contain"
-                  alt="thumbnail"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">MacBook Pro X</p>
-                  <p className="text-sm text-[#616369]">Laptops</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  Rp 24.000.000
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="" className="product-card">
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <img
-                  src="/assets/thumbnails/airpods-max-select-skyblue-202011-Photoroom 1.png"
-                  className="w-full h-full object-contain"
-                  alt="thumbnail"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">Tuli Nyaman</p>
-                  <p className="text-sm text-[#616369]">Headsets</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  Rp 3.500.000.000
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="" className="product-card">
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <img
-                  src="/assets/thumbnails/imac24-digitalmat-gallery-1-202310-Photoroom 1.png"
-                  className="w-full h-full object-contain"
-                  alt="thumbnail"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">
-                    Warna iMac Jadi
-                  </p>
-                  <p className="text-sm text-[#616369]">Desktops</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  Rp 89.000.000
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
+        <Suspense fallback={<span>Loading...</span>}>
+          <ListProducts
+            isShowDetail={false}
+            title={
+              <>
+                Other Products <br /> You Might Need
+              </>
+            }
+          />
+        </Suspense>
       </div>
     </>
   );
